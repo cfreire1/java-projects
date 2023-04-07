482853111e0d572ae9c7bfe033e44923665d4e02
cristopher freire <christ.freire@hotmail.com>
2023-04-07 04:35:51 -0400

    feat: se agrega proyecto que permite listar un archivo de properties, en la consulta de una apiy tambien filtrar por sus propiedades.

---
+++ spring-boot/gradle-spring-boot-rest-info-properties/README.md
@@ -0,0 +1,5 @@
+# Api que permite devolver el contenido de un archivo properties yml o propertiesy buscar por su propiedades
+* Permite devolver el contenido de un archivo properties en estructura json
+* Permite buscar el la propiedad en el proyecto en estructura json
+
+
---
+++ spring-boot/gradle-spring-boot-rest-info-properties/src/test/java/com/example/gradlespringbootrestinfoproperties/GradleSpringBootRestInfoPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlespringbootrestinfoproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringBootRestInfoPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
